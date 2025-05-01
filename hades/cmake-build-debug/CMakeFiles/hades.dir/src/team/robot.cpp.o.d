dfdb4745b80523df7d273b6777ea8da0aff0d857
Thauan Silva <167044466+thafsf@users.noreply.github.com>
2025-05-01 11:36:15 -0300

    Delete hades/cmake-build-debug directory

--- hades/cmake-build-debug/CMakeFiles/hades.dir/src/team/robot.cpp.o.d
+++
@@ -1,4 +0,0 @@
-CMakeFiles/hades.dir/src/team/robot.cpp.o: \
- /home/caiu/Desktop/ssl/Core/hades/src/team/robot.cpp \
- /usr/include/stdc-predef.h \
- /home/caiu/Desktop/ssl/Core/hades/src/team/robot.h
