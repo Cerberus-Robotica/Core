47389644c240e4910758d18fc1efaa80d46f66f0
thafsf <thauan.s2004@aluno.ifsc.edu.br>
2025-05-02 16:13:51 -0300

    adição do gitignore

---
+++ hades/build/CMakeFiles/hades.dir/DependInfo.cmake
@@ -0,0 +1,32 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/cerberus/Desktop/Core/hades/src/c_trajectory/C_trajectory.cpp" "CMakeFiles/hades.dir/src/c_trajectory/C_trajectory.cpp.o" "gcc" "CMakeFiles/hades.dir/src/c_trajectory/C_trajectory.cpp.o.d"
+  "/home/cerberus/Desktop/Core/hades/src/c_trajectory/geometry/Vetop.cpp" "CMakeFiles/hades.dir/src/c_trajectory/geometry/Vetop.cpp.o" "gcc" "CMakeFiles/hades.dir/src/c_trajectory/geometry/Vetop.cpp.o.d"
+  "/home/cerberus/Desktop/Core/hades/src/c_trajectory/geometry/circle.cpp" "CMakeFiles/hades.dir/src/c_trajectory/geometry/circle.cpp.o" "gcc" "CMakeFiles/hades.dir/src/c_trajectory/geometry/circle.cpp.o.d"
+  "/home/cerberus/Desktop/Core/hades/src/c_trajectory/geometry/rectangle.cpp" "CMakeFiles/hades.dir/src/c_trajectory/geometry/rectangle.cpp.o" "gcc" "CMakeFiles/hades.dir/src/c_trajectory/geometry/rectangle.cpp.o.d"
+  "/home/cerberus/Desktop/Core/hades/src/include/handlers.cpp" "CMakeFiles/hades.dir/src/include/handlers.cpp.o" "gcc" "CMakeFiles/hades.dir/src/include/handlers.cpp.o.d"
+  "/home/cerberus/Desktop/Core/hades/src/main.cpp" "CMakeFiles/hades.dir/src/main.cpp.o" "gcc" "CMakeFiles/hades.dir/src/main.cpp.o.d"
+  "/home/cerberus/Desktop/Core/hades/src/team/Robot_controller.cpp" "CMakeFiles/hades.dir/src/team/Robot_controller.cpp.o" "gcc" "CMakeFiles/hades.dir/src/team/Robot_controller.cpp.o.d"
+  "/home/cerberus/Desktop/Core/hades/src/team/geometry/world_model.cpp" "CMakeFiles/hades.dir/src/team/geometry/world_model.cpp.o" "gcc" "CMakeFiles/hades.dir/src/team/geometry/world_model.cpp.o.d"
+  "/home/cerberus/Desktop/Core/hades/src/team/leader.cpp" "CMakeFiles/hades.dir/src/team/leader.cpp.o" "gcc" "CMakeFiles/hades.dir/src/team/leader.cpp.o.d"
+  "/home/cerberus/Desktop/Core/hades/src/team/plays/play_attack.cpp" "CMakeFiles/hades.dir/src/team/plays/play_attack.cpp.o" "gcc" "CMakeFiles/hades.dir/src/team/plays/play_attack.cpp.o.d"
+  "/home/cerberus/Desktop/Core/hades/src/team/plays/play_debug.cpp" "CMakeFiles/hades.dir/src/team/plays/play_debug.cpp.o" "gcc" "CMakeFiles/hades.dir/src/team/plays/play_debug.cpp.o.d"
+  "/home/cerberus/Desktop/Core/hades/src/team/plays/play_goal_keeper.cpp" "CMakeFiles/hades.dir/src/team/plays/play_goal_keeper.cpp.o" "gcc" "CMakeFiles/hades.dir/src/team/plays/play_goal_keeper.cpp.o.d"
+  "/home/cerberus/Desktop/Core/hades/src/team/robot.cpp" "CMakeFiles/hades.dir/src/team/robot.cpp.o" "gcc" "CMakeFiles/hades.dir/src/team/robot.cpp.o.d"
+  "/home/cerberus/Desktop/Core/hades/src/team/team_info.cpp" "CMakeFiles/hades.dir/src/team/team_info.cpp.o" "gcc" "CMakeFiles/hades.dir/src/team/team_info.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
