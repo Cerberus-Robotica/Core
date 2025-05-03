a6ceefdcd35679dbb7c010885c0258c46fae2831
thafsf <thauan.s2004@aluno.ifsc.edu.br>
2025-05-03 08:59:30 -0300

    bug_fix_gitignore

--- hades/build/CMakeFiles/hades.dir/src/team/team_info.cpp.o.d
+++
@@ -1,4 +0,0 @@
-CMakeFiles/hades.dir/src/team/team_info.cpp.o: \
- /home/cerberus/Desktop/Core/hades/src/team/team_info.cpp \
- /usr/include/stdc-predef.h \
- /home/cerberus/Desktop/Core/hades/src/team/team_info.h
