1592295701f298335ef84b00f144829c86aa5367
thafsf <thauan.s2004@aluno.ifsc.edu.br>
2025-07-14 10:45:44 -0300

    bug fix external/lecm

--- build_core.sh
+++ build_core.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -e
 
-
+git submodule update --init --recursive
 
 echo " Instalando Caronte... 🛶, Hermes... ⚚, Hades... 🐺"
 
--- iris/src/main.py
+++ iris/src/main.py
@@ -1,5 +1,7 @@
-import lcm
+import sys
 import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../../')))
+from build.python import lcm
 from data import *
 import time
 
